fd0953dd964641cbeada91ea8b8af55c0fd55cbc
Mani Kumar Donepudi <56865239+manikumar999@users.noreply.github.com>
2020-07-04 13:06:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd
+import os
+os.chdir(os.path.dirname(__file__))
+import pickle
+
+from pretty_html_table import build_table
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template("mainpage_V2.html")
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    
+    model = pickle.load(open('model.pkl','rb'))
+    data = pd.read_csv("list_data.csv")
+    data = data.iloc[:,2:]
+    data.fillna('b',inplace=True)
+    values = dict((key, request.form.getlist(key)) for key in request.form.keys())
+    
+        
+    
+    testdf = pd.DataFrame(values)
+    cols = list(data.columns)
+    testdf = testdf.loc[:,cols]
+    data = pd.concat([data,testdf])
+    data = pd.get_dummies(data)
+    test = data.iloc[-1].values.reshape(1, -1)
+    testpred = model.predict(test)
+    # pht =build_table(testdf.T,"blue_light")
+    return render_template("predict.html",values=testdf.T.to_html(),pred = testpred[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True,port = 6000)
+  
+
+
+
+
+
+
